bf40f88762b0fe2f5571b77c415d15c16ba47ea6
朱福利 <hzzhufuli@corp.netease.com>
2020-08-21 08:09:19 +0800

    118

---
+++ arrary/_118YHTriangles.java
@@ -0,0 +1,13 @@
+class YX{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class YXT {
+    public int[][] generate(int numRows) {
+        int[][] result = new int[numRows][numRows];
+        
+        return result;
+    }
+}
