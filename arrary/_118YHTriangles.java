4b046d628a80037ecead22289cd654fa402ab2b5
朱福利 <hzzhufuli@corp.netease.com>
2020-08-21 08:52:34 +0800

    118 new

--- arrary/_118YHTriangles.java
+++ arrary/_118YHTriangles.java
@@ -1,13 +1,32 @@
-class YX{
+import java.util.ArrayList;
+import java.util.List;
+
+class _118YHTriangles{
     public static void main(String[] args) {
-        
+        YXT solution = new YXT();
+        solution.generate(5);
     }
 }
 
 class YXT {
-    public int[][] generate(int numRows) {
-        int[][] result = new int[numRows][numRows];
-        
+    public List<List<Integer>> generate(int numRows) {
+        if(numRows<=0){
+            return new ArrayList();
+        }
+        List<List<Integer>> result = new ArrayList();
+        List<Integer> row = new ArrayList();
+        row.add(1);
+        result.add(row);
+
+        for (int i = 1; i < numRows; i++) {
+            row = new ArrayList();
+            row.add(1);// 
+            for (int j = 1; j < i; j++) {
+                row.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+            }
+            row.add(i, 1);
+            result.add(row);
+        }
         return result;
     }
 }
