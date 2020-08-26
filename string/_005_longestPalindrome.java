9b54e4288d724707d02bf576ef0f90c72df47c11
朱福利 <hzzhufuli@corp.netease.com>
2020-08-26 20:13:29 +0800

    005 key修改

--- string/_005_longestPalindrome.java
+++ string/_005_longestPalindrome.java
@@ -4,14 +4,32 @@
  */
 public class _005_longestPalindrome {
     public static void main(String[] args) {
-        
+        longestPalindrome("s");
     }
 
-    public static String  longestPalindrome() {
+    public static String longestPalindrome(String s) {
+        String ans = "";
+        int max = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j <= len; j++) {
+                String test = s.substring(i, j);
+                if (isPalindromic(test) && test.length() > max) {
+                    ans = s.substring(i, j);
+                    max = Math.max(max, ans.length());
+                }
+            }
+        }
+        return ans;
+    }
 
-
-
-
-        return "";
+    public static boolean isPalindromic(String s) {
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
