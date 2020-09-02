41dbedc6233312e48e1800759ea5ea99740c6bec
朱福利 <hzzhufuli@corp.netease.com>
2020-09-02 20:31:47 +0800

    005

--- code/string/_005_longestPalindrome.java
+++ code/string/_005_longestPalindrome.java
@@ -40,27 +40,27 @@ public class _005_longestPalindrome {
     }
 
 
-    public static String longestPalindromeOpt(String s) {
-        if (s == null || s.length() < 1) return "";
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = isPalindromicOpt(s, i, i);// 对奇数位判断
-            int len2 = isPalindromicOpt(s, i, i + 1);// 对偶数位判断
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+public static String longestPalindromeOpt(String s) {
+    if (s == null || s.length() < 1) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int len1 = isPalindromicOpt(s, i, i);// 对奇数位判断
+        int len2 = isPalindromicOpt(s, i, i + 1);// 对偶数位判断
+        int len = Math.max(len1, len2);// 去他们之间最大的
+        if (len > end - start) {// 
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
         }
-        return s.substring(start, end + 1);
     }
-    
-    private static int isPalindromicOpt(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
-        }
-        return R - L - 1;
+    return s.substring(start, end + 1);
+}
+
+private static int isPalindromicOpt(String s, int left, int right) {
+    int L = left, R = right;
+    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+        L--;
+        R++;
     }
+    return R - L - 1;
+}
 }
